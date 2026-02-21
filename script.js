6ce020ceae8851d619b858dd282d1cbbe4c9480d
abiral <abiralbasistadawadi@gmail.com>
2026-02-21 22:06:26 +0545

    you are the king of git and github

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ console.log(button);
 
 const login="login page added";
 console.log(login);
+
+
+const payment="Integreated the payment gatwway";
+console.log(payment);
+
+
+
+
+const upi="upi integreaated";
+console.log(upi);
+
+
+const footer="footer aayo";
+
+console.log(footer);
+
+
+console.log("lastest update changes");
